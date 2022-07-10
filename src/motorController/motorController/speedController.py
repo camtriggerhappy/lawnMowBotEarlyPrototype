eae48945bb48c001f4f94030f68be99c1d55cb03
cameron <ckiw23@gmail.com>
2022-07-10 16:31:38 -0400

    you need both for it to run at all

--- src/motorController/motorController/speedController.py
+++ src/motorController/motorController/speedController.py
@@ -7,13 +7,13 @@ def main():
 class differentialDrive():
     def __init__(self):
         self.h = lgpio.gpiochip_open(0)
-        self.rightMotor = 12
-        self.leftMotor = 33
+        self.rightMotorSpeed = 15
+        self.rightMotorDirectiom = 14
         
         
     def forward(self):
-        lgpio.gpio_write(self.h, self.leftMotor,1)
-        lgpio.gpio_write(self.h, self.rightMotor,1)
+        lgpio.gpio_write(self.h, self.rightMotorSpeed,0)
+        lgpio.gpio_write(self.h, self.rightMotorDirectiom,1)
 
 
 if __name__ == '__main__':
