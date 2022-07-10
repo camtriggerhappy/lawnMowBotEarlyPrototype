1376ae31ae1ef6fa8f5ce1383321404990ba8682
cameron <ckiw23@gmail.com>
2022-07-10 15:29:31 -0400

    motor test

---
+++ src/motorController/motorController/speedController.py
@@ -0,0 +1,20 @@
+import lgpio
+
+def main():
+    diff = differentialDrive
+    diff.forward()
+
+class differentialDrive():
+    def __init__(self):
+        self.h = lgpio.gpiochip_open(0)
+        self.rightMotor = 12
+        self.leftMotor = 33
+        
+        
+    def forward(self):
+        lgpio.tx_pwm(self.h, self.leftMotor,80)
+        lgpio.tx_pwm(self.h, self.rightMotor,80)
+
+
+if __name__ == '__main__':
+    main()
