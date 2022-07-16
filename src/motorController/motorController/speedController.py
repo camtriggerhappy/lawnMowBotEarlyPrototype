3dd9d0f61c11542ef60cada745b9167f0ba337e8
cameron <ckiw23@gmail.com>
2022-07-16 13:09:57 -0400

    flipped pin numbers

--- src/motorController/motorController/speedController.py
+++ src/motorController/motorController/speedController.py
@@ -13,21 +13,22 @@ def main():
 class differentialDrive():
     def __init__(self, h):
         self.h = h
-        self.rightMotorSpeed = 15
-        self.rightMotorDirectiom = 14
+        self.rightMotorSpeed = 14
+        self.rightMotorDirectiom = 15
         
-        self.leftMotorSpeed = 23
-        self.leftMotorDirection = 24
+        self.leftMotorSpeed = 24
+        self.leftMotorDirection = 23
         
         
         
         
     def forward(self):
-        lgpio.tx_pwm(self.h, self.rightMotorSpeed,120, 20)
+        lgpio.tx_pwm(self.h, self.rightMotorSpeed,120,pwm_duty_cycle=20)
         lgpio.gpio_write(self.h, self.rightMotorDirectiom,1)
         
-        lgpio.tx_pwm(self.h, self.leftMotorSpeed,120,  20)
+        lgpio.tx_pwm(self.h, self.leftMotorSpeed,120, pwm_duty_cycle=20)
         lgpio.gpio_write(self.h, self.leftMotorDirection, 1)
+        print("moving")
 
 
 if __name__ == '__main__':
