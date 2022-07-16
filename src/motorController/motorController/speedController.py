dce25f62507e6e444b0e316adb6ceaf8675346ab
cameron <ckiw23@gmail.com>
2022-07-16 11:38:05 -0400

    speed controller now controls both sides

--- src/motorController/motorController/speedController.py
+++ src/motorController/motorController/speedController.py
@@ -1,22 +1,34 @@
+
 import lgpio
 
 def main():
-    diff = differentialDrive()
+    handler = lgpio.gpiochip_open(0)
+    diff = differentialDrive(handler)
     while True:
         diff.forward()
+        
+    lgpio.gpiochip_close(0)
+        
 
 class differentialDrive():
-    def __init__(self):
-        self.h = lgpio.gpiochip_open(0)
+    def __init__(self, h):
+        self.h = h
         self.rightMotorSpeed = 15
         self.rightMotorDirectiom = 14
         
+        self.leftMotorSpeed = 23
+        self.leftMotorDirection = 24
+        
+        
+        
         
     def forward(self):
-        lgpio.tx_pwm(self.h, self.rightMotorSpeed,120, 80)
+        lgpio.tx_pwm(self.h, self.rightMotorSpeed,120, 20)
         lgpio.gpio_write(self.h, self.rightMotorDirectiom,1)
+        
+        lgpio.tx_pwm(self.h, self.leftMotorSpeed,120,  20)
+        lgpio.gpio_write(self.h, self.leftMotorDirection, 1)
 
 
 if __name__ == '__main__':
     main()
-s
