2f15dda9a7172b0c58b1b60a3e76788acce03d76
cameron <ckiw23@gmail.com>
2022-08-18 18:54:04 -0400

    odom

--- src/robotController/robotController/__init__.py
+++ src/robotController/robotController/__init__.py
@@ -37,6 +37,7 @@ class driveController(Node):
         self.yaw = 0
         self.roll = 0
         self.previousCallTime = self.get_clock().now()
+        self.odom = Odometry()
         
         timer_period = 0.1  # seconds
         self.timer = self.create_timer(timer_period, self.updatePose)
