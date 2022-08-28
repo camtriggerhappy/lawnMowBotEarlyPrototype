aabb669e779be2c14663a919347cf99329713625
cameron <ckiw23@gmail.com>
2022-08-28 15:11:16 -0400

    another typo

--- scripts/launchBot.sh
+++ scripts/launchBot.sh
@@ -2,4 +2,4 @@ source /opt/ros/humble/setup.bash
 source ~/lawnMowBot/install/setup.bash
 source ~/rpLidarROS2/rplidarTest/rplidar_ros2/install/setup.bash
 
-ros2 launch nav2_bringup navigation_launch.py && ros2 launch slam_toolbox online_asynch_launch.py
+ros2 launch nav2_bringup navigation_launch.py && ros2 launch slam_toolbox online_async_launch.py
